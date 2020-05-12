4b04a0eaded7aeaf98ef9e6cdfbecd4589f2259d
tomMisson <11tmisson@gmail.com>
2020-05-12 14:47:57 +0100

    Add default endpoint

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ require('dotenv').config()
 var express = require('express')
 var app = express()
 
+app.get('/', (req,res)=>{
+    res.sendStatus(200)
+})
+
 app.post('/orders', async (req,res)=>{
     console.log()
 })
