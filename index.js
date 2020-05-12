ef408b909d566ff8a5bef07f161313e9b02b1d72
tomMisson <11tmisson@gmail.com>
2020-05-12 15:03:02 +0100

    Show body

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ require('dotenv').config()
 var express = require('express')
 var app = express()
 
+app.use(express.json())
+
 app.get('/', (req,res)=>{
     res.sendStatus(200)
 })
 
 app.post('/orders', async (req,res)=>{
-    console.log()
+    res.json({"response":res.body})
 })
 
 app.listen(process.env.PORT, console.log(`App running on ${process.env.PORT}`))
