e4582b98afc8418b28d05b9503e35a8c2225a418
tomMisson <11tmisson@gmail.com>
2020-05-13 17:05:27 +0100

    Fixed duplicate rows issue and added dump save aswell

--- index.js
+++ index.js
@@ -13,28 +13,39 @@ app.get('/', (req,res)=>{
 
 app.post('/orders', async (req,res)=>{
 
-    let data = req.body
+    let data={
+        PartitionKey : {'_':req.body.number},
+        RowKey : {'_':req.body.number},
+        content : {'_':JSON.stringify(req.body)}
+    };
+
+    tableSvc.insertEntity('ordersDump',data, function (error, result, response) {
+        if(!error){
+        }
+    });
+    
+    data = req.body;
 
     let customer = JSON.stringify(data.shipping)
-    let items = data.line_items
+    let items = data.line_items 
 
-    for(var i; i<items.length; i++)
+    for(var i=0; i<items.length; i++)
     {
         let dataToSave = {
             PartitionKey: {'_':data.number},
-            RowKey: {'_':data.number+" " +i},
+            RowKey: {'_':i+""},
             shippingInfo: {'_':customer},
-            orderedItems: {'_':items},
+            orderedItems: {'_':JSON.stringify(items[i])},
         }
 
         tableSvc.insertEntity('orders',dataToSave, function (error, result, response) {
             if(!error){
-              res.sendStatus(200)
             }
             else
                 res.json(error)
         });
     }
+    res.sendStatus(200)
 })
 
 app.listen(process.env.PORT, console.log(`App running on ${process.env.PORT}`))
